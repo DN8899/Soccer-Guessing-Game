1bfd557626bb6d5017c8d534e7506dcc23ac0c29
Derrick <derricknovak889@gmail.com>
2023-07-12 17:19:14 -0400

    Started adding Query.

--- SoccerPredictionGame/src/main/java/com/example/SoccerPredictionGame/player/PlayerRepository.java
+++ SoccerPredictionGame/src/main/java/com/example/SoccerPredictionGame/player/PlayerRepository.java
@@ -3,10 +3,13 @@ package com.example.SoccerPredictionGame.player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /*
 Repo used to access the data
  */
 @Repository
 public interface PlayerRepository
         extends JpaRepository<Player, Long> {
+    Optional<Player> findPlayerByUserName(String userName);
 }
